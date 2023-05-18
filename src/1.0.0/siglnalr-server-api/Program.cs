257c0c0d8e2ccd9310e2e59e8ab680a71c8bfaf3
vince.j.del.rosario <vince.j.del.rosario@accenture.com>
2023-05-18 20:48:37 +0800

    1.0.0 | backend | add CORS policy

--- src/1.0.0/siglnalr-server-api/Program.cs
+++ src/1.0.0/siglnalr-server-api/Program.cs
@@ -4,6 +4,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSignalR();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+
+    /*
+    // adding a custom policy
+
+    options.AddPolicy(name: "AllowedSpecificOrigins",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+    */
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -11,6 +32,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors();
+
+/*
+// using a custom policy
+
+app.UseCors("AllowedSpecificOrigins");
+*/
 
 app.UseAuthorization();
 
